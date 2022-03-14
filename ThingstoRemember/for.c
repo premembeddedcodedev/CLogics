cfa582872ef8c1cdd3c08c5157800f7cbd082a1a
praveenv <prem441@gmail.com>
2022-03-14 17:52:08 +0530

    [Things to remember] Adding for loop pre-post inc

---
+++ ThingstoRemember/for.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	printf("\n");
+	
+	printf("pre inc values are: \n");
+	for(i=0; i<8; ++i)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	printf("post inc values are: \n");
+	for(i=0; i<8; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+
+	return 0;
+}

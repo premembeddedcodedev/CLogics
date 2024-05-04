d280c95ebda6b98119d48dba206b17c049b53010
Praveen Vuppala <prem441@gmail.com>
2024-05-04 20:11:44 +0530

    LeethCode problems added

---
+++ Leetcode/Strings/optimised/LC3_Longest_Substring_Without_Repeating_Characters_opt.c
@@ -0,0 +1,61 @@
+/*
+ * Given a string s, find the length of the longest substring
+ without repeating characters.https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+ Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+*/
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+
+int lengthOfLongestSubstring(char* s)
+{
+	char hashset[256] = {-1};
+	int maxlen = INT_MIN;
+	int l = 0, r = 0;
+
+	memset(hashset, 0, sizeof(hashset));
+
+	for(r=0; r<strlen(s); r++) {
+		if((hashset[s[r]] != -1) && hashset[s[r]] >= l) 
+			l = hashset[s[r]] + 1;
+
+		if(maxlen < (r-l+1))
+			maxlen = r - l + 1;
+
+		hashset[s[r]] = r;
+	}
+
+	return maxlen;
+}
+
+int main()
+{
+	char *ptr = "bbbbbb";//"cadbzabcd";//"abcabcbb";
+
+	printf("length: %d\n",lengthOfLongestSubstring(ptr));
+
+	return 0;
+}
+
